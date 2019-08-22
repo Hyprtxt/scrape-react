0301275953d7bb3341e88fb2aca0da8b653ca0ce
Taylor Y <taylor@cruzscience.com>
2019-08-22 11:58:26 -0700

    good

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import request from "superagent";
+import useFetch from "hooks";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
+const Data = () => {
+  const [data, loading] = useFetch(
+    "http://localhost:3000/storefronts/emerald-phog-san-bruno"
+  );
+  return (
+    <React.Fragment>
+      <h1>THINGS</h1>
+      {loading ? "Loading..." : <pre>{JSON.stringify(data, null, 2)}</pre>}
+    </React.Fragment>
+  );
+};
+
+const Categories = () => {
+  const [data, loading] = useFetch(
+    "http://localhost:3000/storefronts/emerald-phog-san-bruno"
+  );
+  const renderLoading = () => <p>Loading&hellip;</p>;
+  const renderWithData = () => {
+    const cats = data[0].products.map(product => {
+      return product.category.slug;
+    });
+    return <pre>{JSON.stringify(cats, null, 2)}</pre>;
+  };
+  return loading ? renderLoading() : renderWithData();
+};
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,8 +48,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <Categories />
+      <Data />
     </div>
   );
-}
+};
 
 export default App;
